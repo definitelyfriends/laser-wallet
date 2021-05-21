c68376e1880b231afe441e3a0baead202cd720af
David Gonzalez <dave@bitshifter.co>
2021-05-21 12:28:01 -0400

    initial commit

---
+++ src/pages/Options/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from 'react-dom';
+import 'assets/styles/tailwind.css';
+
+import Options from './Options.tsx';
+import './index.css';
+
+render(
+  <Options title={'settings'} />,
+  window.document.querySelector('#app-container')
+);
+
+if (module.hot) module.hot.accept();
