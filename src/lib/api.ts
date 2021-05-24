f2409810abbee5f62d97569849b75fcacc55e786
David Gonzalez <dave@bitshifter.co>
2021-05-24 14:22:36 -0400

    little bit of cleanup

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,8 +1,23 @@
 const baseUrl = 'https://api.helium.io/v1';
 const accounts = `${baseUrl}/accounts`;
 
+const HOTSPOT = 'hotspot';
+const OUIS = 'ouis';
+const ACTIVITY = 'activity';
+const ACTIVITY_COUNT = 'activity/count';
+const ELECTIONS = 'elections';
+const CHALLENGES = 'challenges';
+const PENDING_TXNS = 'pending_transactions';
+const REWARDS = 'rewards';
+const REWARD_TOTALS = 'rewards/sum';
+const STATS = 'stats';
+
+const generateAddressUrl = (address: string, type = '') =>
+  `${accounts}/${address}/${type}`;
+
 export const fetchAccount = async (address: string) => {
-  const response = await fetch(`${accounts}/${address}`);
+  const accountUrl = generateAddressUrl(address);
+  const response = await fetch(accountUrl);
 
   if (!response.ok) {
     throw new Error('Network response was not ok');
@@ -12,7 +27,9 @@ export const fetchAccount = async (address: string) => {
 };
 
 export const fetchAccountHotspots = async (address: string) => {
-  const response = await fetch(`${accounts}/${address}/hotspots`);
+  const ouiUrl = generateAddressUrl(address, OUIS);
+  const response = await fetch(ouiUrl);
+
   if (!response.ok) {
     throw new Error('Network response was not ok');
   }
