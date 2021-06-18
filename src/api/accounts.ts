f8adbf46ae3e33837f1037e842e1966b7c7a3f20
David Gonzalez <dave@bitshifter.co>
2021-06-18 17:38:26 -0400

    set enum

--- src/api/accounts.ts
+++ src/api/accounts.ts
@@ -1,34 +1,23 @@
 import { useQuery } from 'react-query';
 import { fetchItem } from 'lib/store';
 
-const HOTSPOTS = 'hotspots';
-const OUIS = 'ouis';
-const ACTIVITY = 'activity';
-const ACTIVITY_COUNT = 'activity/count';
-const ELECTIONS = 'elections';
-const CHALLENGES = 'challenges';
-const PENDING_TXNS = 'pending_transactions';
-const REWARDS = 'rewards';
-const REWARD_TOTALS = 'rewards/sum';
-const STATS = 'stats';
-
 const baseUrl = 'https://api.helium.io/v1';
 const accounts = `${baseUrl}/accounts`;
 
-// enum EndpointType {
-//   HOTSPOTS = 'HOTSPOTS',
-//   OUIS = 'OUIS',
-//   ACTIVITY = 'ACTIVITY',
-//   ACTIVITY_COUNT = 'ACTIVITY_COUNT',
-//   ELECTIONS = 'ELECTIONS',
-//   CHALLENGES = 'CHALLENGES',
-//   PENDING_TXNS = 'PENDING_TXNS',
-//   REWARDS = 'REWARDS',
-//   REWARD_TOTALS = 'REWARD_TOTALS',
-//   STATS = 'STATS',
-// }
+enum Endpoint {
+  HOTSPOTS = 'hotspots',
+  OUIS = 'OUIS',
+  ACTIVITY = 'ACTIVITY',
+  ACTIVITY_COUNT = 'ACTIVITY_COUNT',
+  ELECTIONS = 'ELECTIONS',
+  CHALLENGES = 'CHALLENGES',
+  PENDING_TXNS = 'PENDING_TXNS',
+  REWARDS = 'REWARDS',
+  REWARD_TOTALS = 'REWARD_TOTALS',
+  STATS = 'STATS',
+}
 
-export const fetchAccountBase = async (type = '') => {
+export const fetchAccountBase = async (type?: Endpoint) => {
   const address = await fetchItem('address');
   const accountUrl = `${accounts}/${address}/${type}`;
   const response = await fetch(accountUrl);
@@ -45,41 +34,41 @@ export const fetchAccount = () => {
 };
 
 export const fetchAccountHotspots = () => {
-  return useQuery('accountHotspots', () => fetchAccountBase(HOTSPOTS));
+  return useQuery('accountHotspots', () => fetchAccountBase(Endpoint.HOTSPOTS));
 };
 
 export const fetchAccountOuis = () => {
-  return useQuery('accountOUIs', () => fetchAccountBase(OUIS));
+  return useQuery('accountOUIs', () => fetchAccountBase(Endpoint.OUIS));
 };
 
 export const fetchAccountActivity = () => {
-  return useQuery('accountActivity', () => fetchAccountBase(ACTIVITY));
+  return useQuery('accountActivity', () => fetchAccountBase(Endpoint.ACTIVITY));
 };
 
 export const fetchAccountActivityCount = () => {
-  return useQuery('accountActivityCount', () => fetchAccountBase(ACTIVITY_COUNT));
+  return useQuery('accountActivityCount', () => fetchAccountBase(Endpoint.ACTIVITY_COUNT));
 };
 
 export const fetchAccountElections = () => {
-  return useQuery('accountElections', () => fetchAccountBase(ELECTIONS));
+  return useQuery('accountElections', () => fetchAccountBase(Endpoint.ELECTIONS));
 };
 
 export const fetchAccountChallenges = () => {
-  return useQuery('accountChallenges', () => fetchAccountBase(CHALLENGES));
+  return useQuery('accountChallenges', () => fetchAccountBase(Endpoint.CHALLENGES));
 };
 
 export const fetchAccountPendingTxns = () => {
-  return useQuery('accountPendingTxns', () => fetchAccountBase(PENDING_TXNS));
+  return useQuery('accountPendingTxns', () => fetchAccountBase(Endpoint.PENDING_TXNS));
 };
 
 export const fetchAccountRewards = () => {
-  return useQuery('accountRewards', () => fetchAccountBase(REWARDS));
+  return useQuery('accountRewards', () => fetchAccountBase(Endpoint.REWARDS));
 };
 
 export const fetchAccountRewardTotals = () => {
-  return useQuery('accountRewardTotals', () => fetchAccountBase(REWARD_TOTALS));
+  return useQuery('accountRewardTotals', () => fetchAccountBase(Endpoint.REWARD_TOTALS));
 };
 
 export const fetchAccountStats = () => {
-  return useQuery('accountStats', () => fetchAccountBase(STATS));
+  return useQuery('accountStats', () => fetchAccountBase(Endpoint.STATS));
 };
