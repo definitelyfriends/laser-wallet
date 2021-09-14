6035b72873c5b4ad5a4ed2b5f958d9c0fbf06126
David Gonzalez <dave@bitshifter.co>
2021-09-14 10:57:09 -0400

    add recreate keypair function

--- src/lib/vault.utils.ts
+++ src/lib/vault.utils.ts
@@ -1,7 +1,15 @@
 import { split, map } from 'ramda';
 
 import * as buffer from 'buffer';
+import { Keypair } from '@helium/crypto';
 (window as any).Buffer = buffer.Buffer;
+
+const KEY_TYPE = 'ed25519';
+
+interface KeypairProps {
+  pk: string;
+  secretKey: string;
+}
 
 export const lowercase = (word: string) => word.toLowerCase();
 
@@ -10,3 +18,12 @@ export const convertToArray = (seedPhrase: string): string[] => {
 };
 
 export const toBase64 = (key: Uint8Array): string => Buffer.from(key).toString('base64');
+
+export const toBuffer = (key: string): Uint8Array => Buffer.from(key, 'base64');
+
+export const createKeypair = ({ pk, secretKey }: KeypairProps): Keypair => {
+  const publicKey = toBuffer(pk);
+  const privateKey = toBuffer(secretKey);
+
+  return new Keypair({ publicKey, privateKey, keyType: KEY_TYPE });
+};
